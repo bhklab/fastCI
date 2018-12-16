6bfaf4021a233e90f91e488edde09be5c1d76ac7
Ian Smith <iamsinht@gmail.com>
2018-12-16 17:59:50 -0500

    Initial changes to allow computing CI with ties.  Includes polynomial math and a method to generate the product of polynomials of the form (1,1,1,1...,1) using a difference of CDF method that is O(n) as opposed to O(n^2)(?) of the polynom library for generalized polynomial products.  This will dramatically speed up computing the null distribution.  CI with ties is not yet completely implemented.

--- computeNullCI.R
+++ computeNullCI.R
@@ -1,6 +1,8 @@
 # Make CI null distribution table
 # Derives from https://cs.uwaterloo.ca/journals/JIS/VOL4/MARGOLIUS/inversions.pdf
 
+library(polynom)
+
 #' Function makeNullCIDist: Computes the distribution of CI under the assumption that all permutations are equally likely.
 #' Parameters: 
 #'     n:        positive integer, length of the sequence, i.e. the number of elements
@@ -37,7 +39,6 @@ makeNullCIDist <- function(n, makeplot=0, outdir="", cumulative=1){
 }
 
 
-
 getCIPvals <- function(nullCIDist, n, discpairs, alternative=c("two.sided", "greater", "less")){
   if (length(nullCIDist) != choose(n, 2)+1){
     return("Error: length of input nullCIDist does not match n (should be choose(n,2)+1)")
@@ -58,4 +59,38 @@ getCIPvals <- function(nullCIDist, n, discpairs, alternative=c("two.sided", "gre
             "less" = nullCIDist[discpairs+1]
     )
   }
+}
+
+# library(polynom) does allow operations on polynomials, but it is very slow
+# compared to the CDF-trick for convolutions of c(1,1,1,...,1) implemented
+# for the tie-less case in makeNullCIDist.
+# getMultiplicityPoly computes the D_M(x) = [alpha alpha_{n}}]_{x} polynomial
+# Note that alpha = sum_{i =1:n} alpha_{i} - i.e. it includes this additional
+# multiplicity.  
+# This function turns out to be basically unnecessary.
+getMultiplicityPoly <- function(elements, multiplicity){
+  num <- getSimplePolyProduct(elements, multiplicity)
+  denom <- getSimplePolyProduct(multiplicity, multiplicity)
+  
+  return(as.numeric(as.polynomial(num)/denom))
+}
+
+# This function uses the convolutional trick to multiply k polynomials of the
+# form (1+x+x^2+...x^m) from m=n-k+1 to n
+getSimplePolyProduct <- function(elts, range){
+  if (range > elts){
+    return("Error: Range (or number of polynomials) is greater than the input
+           number of elements")}
+  
+  #initialize - initial length is sum of (elts-1):(elts-range) + 1
+  retpoly <- c(rep(1, elts), rep(0, range/2 * (2*elts - range - 1) + 1 - elts))
+  count <- elts
+  if (range > 1){
+    for (k in (elts-1):(elts-range+1)){
+      retpoly[1:(count+k)] = cumsum(c(retpoly[1:count], rep(0,k))) - 
+                               cumsum(c(rep(0,k), retpoly[1:count]))
+      count <- count + k-1
+    }
+  }
+  return(retpoly[1:count])
 }
