27ea84ca2bbf2ed8613f6cf10bf40f432d501e4d
Petr Smirnov <psmirnov2000@gmail.com>
2018-12-26 19:56:51 -0500

    need to commit to pull changes

--- enumerate_inversions_with_rep.R
+++ enumerate_inversions_with_rep.R
@@ -4,7 +4,7 @@
 library(gtools)
 source("~/Code/fastCI/fastCI.R")
 
-my.nums <- c(1,1,1,4,5, 5, 5, 8, 9, 10, 11, 12)
+my.nums <- c(1,1,1,1,5, 5, 5, 8, 9, 10, 11, 12)
 
 for (i in 7:7){
   myx <- permutations(i, i)
@@ -26,3 +26,41 @@ source("~/Code/fastCI/computeNullCI.R")
 
 
 makeNullCIDist(7, c(1,3,3),cumulative = 0)*factorial(7)
+
+
+## Need to figure out what the hell is the distibution for this: x = [1 2 2 3 3 4] and y = [5 5 6 6 7 8]
+
+x <- c(1,2,2,3,3,4)
+y <- c(5,5,6,6,7,8)
+computeInversionsNumber <- function(x,y){
+  # order.y <- order(y)
+  # y <- y[order.y]
+  # x <- x[order.y]
+  numInv <- 0
+  for(i in 1:length(x)){
+    for(j in i:length(x)){
+      if(x[i] > x[j] & y[i] != y[j]){
+        numInv = numInv + 1
+      }
+    }
+  }
+  return(numInv)
+}
+
+
+for (i in 6:6){
+  myx <- permutations(i, i)
+  # print(myx)
+  res <- apply(myx, 1, function(myx){
+    cur.x <- x[myx]
+    cur.y <- y[myx]
+    return(computeInversionsNumber(cur.x, cur.y))
+  })
+  print(table(res))
+  hist(res) 
+  # print(res)
+}
+
+
+
+
--- tie_distributions.R
+++ tie_distributions.R
@@ -1,3 +1,62 @@
+
+plotTriangle <- function(x, y, sort.order = c("y", "x")){
+  sort.order <- match.arg(sort.order)
+  if(sort.order == "x"){
+    y.temp <- y
+    y <- x
+    x <- y.temp
+  }
+  toRemove <- is.na(x) | is.na(y)
+  x <- x[!toRemove]
+  y <- y[!toRemove]
+  
+  y.order <- order(y)
+  x <- x[y.order]
+  y <- y[y.order]
+  incomparable.y <- list()
+  for(dup in unique(y[duplicated(y)])){
+    idx <- which(y==dup)
+    incomparable.y <- c(incomparable.y,list(apply(combinations(length(idx),2), 1, function(x){
+      idx[x]
+    })))
+  }
+  incomparable.x <- list()
+  for(dup in unique(x[duplicated(x)])){
+    idx <- which(x==dup)
+    incomparable.x <- c(incomparable.x,list(apply(combinations(length(idx),2), 1, function(x){
+      idx[x]
+    })))
+  }
+  
+  rect.y <- matrix(0,length(y),length(y))
+  for(ii in 1:length(y)){
+    rect.y[ii,ii] <- 1
+  }
+  for(kk in seq_along(incomparable.y)){
+    curMatrix <- incomparable.y[[kk]]
+    for(colNum in seq_len(NCOL(curMatrix))){
+      rect.y[curMatrix[1,colNum],curMatrix[2,colNum]] <- 1
+    }
+  }
+  
+  for(kk in seq_along(incomparable.x)){
+    curMatrix <- incomparable.x[[kk]]
+    for(colNum in seq_len(NCOL(curMatrix))){
+      rect.y[curMatrix[1,colNum],curMatrix[2,colNum]] <- rect.y[curMatrix[1,colNum],curMatrix[2,colNum]] + 2
+    }
+  }
+  
+  image(rect.y, col=c("white","blue", "red", "purple"), zlim=c(0,3), xaxt='n', yaxt='n', bty="n")
+  if(sort.order == "y") {
+    legend("bottomright", legend = c("Tied Y", "Tied X", "Tied Both"), fill=c("blue", "red", "purple"))
+  } else {
+    legend("bottomright", legend = c("Tied X", "Tied Y", "Tied Both"), fill=c("blue", "red", "purple"))
+  }
+  
+}
+
+
+
 library(PharmacoGx)
 
 
@@ -33,3 +92,136 @@ test <- apply(rnaseq, 1, function(x) {
 names(test) <- NULL
 
 test <- unique(test)
+
+
+## For arguments sake, taking largest tissue
+my.cells <- na.omit(cellNames(CCLE.CTRPv2)[cellInfo(CCLE.CTRPv2)$tissueid == "lung"])
+
+## We will look at the "triangles" for representitive genes from each quadrant of variance, and also for a broad action and targeted therapy
+
+x.var <- apply(rnaseq, 1, var, na.rm=TRUE)
+
+summary(x.var)
+
+my.order <- order(x.var)
+
+rnaseq <- rnaseq[my.order,]
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1),])
+
+cur.y <- drug.sens["paclitaxel",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_first_quartile_gene_paclitaxel_tie_dist.pdf", onefile=TRUE)
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1)*2,])
+
+cur.y <- drug.sens["paclitaxel",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_second_quartile_gene_paclitaxel_tie_dist.pdf")
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1)*3,])
+
+cur.y <- drug.sens["paclitaxel",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_third_quartile_gene_paclitaxel_tie_dist.pdf")
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1)*4,])
+
+cur.y <- drug.sens["paclitaxel",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_forth_quartile_gene_paclitaxel_tie_dist.pdf")
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+## Now doing worst case scenerio
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1),])
+
+cur.y <- drug.sens["BRD8958",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_first_quartile_gene_BRD8958_tie_dist.pdf", onefile=TRUE)
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1)*2,])
+
+cur.y <- drug.sens["BRD8958",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_second_quartile_gene_BRD8958_tie_dist.pdf")
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1)*3,])
+
+cur.y <- drug.sens["BRD8958",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_third_quartile_gene_BRD8958_tie_dist.pdf")
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+
+cur.x <- Biobase::exprs(rnaseq[sample(floor(length(my.order)/4),1)*3,])
+
+cur.y <- drug.sens["BRD8958",]
+
+
+cur.x <- cur.x[,my.cells]
+cur.y <- cur.y[my.cells]
+
+pdf("lung_forth_quartile_gene_BRD8958_tie_dist.pdf")
+plotTriangle(cur.x, cur.y)
+plotTriangle(cur.x, cur.y, "x")
+dev.off()
+
+
+
